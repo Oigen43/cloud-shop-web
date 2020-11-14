ddffff1139e9fa9f051dede9c0d538d70d5f51a8
Oigen43 <oigen43@icloud.com>
2020-11-14 16:47:58 +0300

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://bmac37yjm8.execute-api.us-east-1.amazonaws.com/dev/products',
   order: 'https://bmac37yjm8.execute-api.us-east-1.amazonaws.com/dev',
-  import: 'https://bmac37yjm8.execute-api.us-east-1.amazonaws.com/dev',
+  import: 'https://bmac37yjm8.execute-api.us-east-1.amazonaws.com/dev/import',
   bff: 'https://bmac37yjm8.execute-api.us-east-1.amazonaws.com/dev'
 };
 
