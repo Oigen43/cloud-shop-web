0c0561bb640025bebcdbc39e376da074126dffe5
Oigen43 <oigen43@icloud.com>
2020-11-23 16:52:08 +0300

    change url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: 'https://bmac37yjm8.execute-api.us-east-1.amazonaws.com/dev/products',
-  order: 'https://bmac37yjm8.execute-api.us-east-1.amazonaws.com/dev',
-  import: 'https://bmac37yjm8.execute-api.us-east-1.amazonaws.com/dev',
-  bff: 'https://bmac37yjm8.execute-api.us-east-1.amazonaws.com/dev'
+  product: 'https://lzpiy14c1m.execute-api.us-east-1.amazonaws.com/dev/products',
+  order: 'https://lzpiy14c1m.execute-api.us-east-1.amazonaws.com/dev',
+  import: 'https://6gsa3gzsv2.execute-api.us-east-1.amazonaws.com/dev/import',
+  bff: 'https://lzpiy14c1m.execute-api.us-east-1.amazonaws.com/dev'
 };
 
 export default API_PATHS;
