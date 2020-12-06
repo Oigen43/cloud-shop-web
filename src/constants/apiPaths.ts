7f5861f7d6110f59cc7e06738943892cd9fb2b7e
Oigen43 <oigen43@icloud.com>
2020-12-06 21:04:44 +0300

    complete task

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,8 @@ const API_PATHS = {
   product: 'https://lzpiy14c1m.execute-api.us-east-1.amazonaws.com/dev/products',
   order: 'https://lzpiy14c1m.execute-api.us-east-1.amazonaws.com/dev',
   import: 'https://6gsa3gzsv2.execute-api.us-east-1.amazonaws.com/dev/import',
-  bff: 'https://lzpiy14c1m.execute-api.us-east-1.amazonaws.com/dev'
+  bff: 'https://lzpiy14c1m.execute-api.us-east-1.amazonaws.com/dev',
+  cart: 'http://oigen43-cart-api-dev.us-east-1.elasticbeanstalk.com/api'
 };
 
 export default API_PATHS;
